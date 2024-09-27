78e2dda07b118663108a86ee32b332690e482881
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2024-09-27 20:17:28 -0300

    🐛fix: Corrige formatação de datas e adiciona tratamento de erros na listagem de receitas

--- backend/src/controllers/controllerReceita.ts
+++ backend/src/controllers/controllerReceita.ts
@@ -62,47 +62,43 @@ export default class ReceitaController {
       };
 
       static async listarReceita (req: Request, res: Response) {
-        const receitaSnapshot = await colecaoReceitas.get();
-        const receitas = receitaSnapshot.docs.map((receita) => {
-            const dataTimestamp = receita.data().data; // Supondo que 'data' seja o campo do timestamp
-    
-            // Converte o timestamp em uma data legível
-            const formattedDate = dataTimestamp.toDate().toLocaleDateString('pt-BR', {
+        try{
+          const receitaSnapshot = await colecaoReceitas.get();
+          const receitas = receitaSnapshot.docs.map((receita) => {
+              const dataTimestamp = receita.data().data; // Supondo que 'data' seja o campo do timestamp
+      
+             // O Firestore retorna a data como string, converta para Date se necessário
+              const formattedDate = new Date(dataTimestamp).toLocaleDateString('pt-BR', {
                 day: '2-digit',
                 month: 'long',
                 year: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-            });
-    
-            return {
-                id: receita.id,
-                ...receita.data() as Omit<Receitas, 'id'>,
-                data: formattedDate, // A data agora está formatada
-                updated_at: receita.data().updated_at.toDate().toLocaleDateString('pt-BR', {
+              });
+      
+              return {
+                  id: receita.id,
+                  ...receita.data() as Omit<Receitas, 'id'>,
+                  data: formattedDate, // A data agora está formatada
+                  created_at: new Date(receita.data().created_at).toLocaleDateString('pt-BR', {
                     day: '2-digit',
                     month: 'long',
                     year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    second: '2-digit',
-                }),
-                created_at: receita.data().created_at.toDate().toLocaleDateString('pt-BR', {
+                  }),
+                  updated_at: new Date(receita.data().updated_at).toLocaleDateString('pt-BR', {
                     day: '2-digit',
                     month: 'long',
                     year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    second: '2-digit',
-                }),
-            };
-        });
-    
-        if (receitas.length === 0) {
-            return res.status(404).json({ message: 'Receita não encontrada' });
+                  }),
+              };
+          });
+      
+          if (receitas.length === 0) {
+              return res.status(404).json({ message: 'Receita não encontrada' });
+          }
+      
+          return res.status(200).json(receitas);
+        } catch (erro) {
+          console.error(erro); // Log do erro para depuração
+          return res.status(500).json({ erro: "Falha ao listar despesas" });
         }
-    
-        return res.status(200).json(receitas);
-    }
+      }
 }
